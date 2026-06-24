8826392e9048a079aa0576965404cde96409d76b
rahmetovtamerlan102-debug <rahmetovtamerlan102@gmail.com>
2026-06-24 15:43:09 +0500

    Create Constants.kt

---
+++ Utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.messenger.utils
+
+object Constants {
+    const val BASE_URL = "https://ваш-домен.onrender.com/api/"
+    const val WS_URL = "wss://ваш-домен.onrender.com/ws/chat/"
+}
